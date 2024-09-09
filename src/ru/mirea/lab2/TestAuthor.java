97e1406dcbd6a0b206a62e9b73113d15928ce1e3
vladimirryab <vladimirryab1003@gmail.com>
2024-09-09 23:39:37 +0300

    first commit :)

---
+++ src/ru/mirea/lab2/TestAuthor.java
@@ -0,0 +1,8 @@
+package ru.mirea.lab2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author author = new Author("Владимир", "vladimirryab1003@gmail.com", 'm');
+        System.out.println(author);
+    }
+}
--- src/ru/mirea/lab2/second.java
+++
@@ -1,7 +0,0 @@
-package ru.mirea.lab2;
-
-public class second {
-    public static void main(String[] args) {
-
-    }
-}
